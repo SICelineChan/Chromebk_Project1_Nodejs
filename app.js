e0db10bfdf6c61569dc71d7707df68b8127abfee
Celine Chan <cctinlone@gmail.com>
2023-01-06 23:06:43 +0100

    added json and ejs

--- app.js
+++ app.js
@@ -4,12 +4,23 @@ const app = express();
 const cors = require("cors");
 //const router = require("./routes/router");
 
-app.use(cors());
+const travelled = require("./travelData.json");
 
+app.use(cors());
 const port = 3000;
 
 app.use(express.json());
 //app.use(router);
+
+app.set(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+
+    const templateData = {countries : travelled,};
+    console.log(travelled);
+    res.render("main", templateData);
+})
 
 app.listen(port, function (){
     console.log(`now listening on port ${port}`);
